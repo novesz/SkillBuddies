9f3941b4d5a5247879d86327480de8568dc72b06
novesz <novesz831@hengersor.hu>
2025-11-03 10:28:35 +0100

    HubiDolgozzal

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header>
+      <span className="dot"></span>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import Header from "../components/Header";
+import React from "react";
 
-const Home = () => {
+
+function Home() {
   return (
     <>
-    <div>Home Page</div>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et sed praesentium ex quis dolorem beatae magni est alias aliquam cumque, cupiditate nulla porro veniam impedit dolore magnam saepe soluta consequatur.</p>
+        <Header />
+        <h1>Welcome to SkillBuddies</h1>
     </>
-  )
+  );
 }
 
-export default Home
+
+export default Home;
