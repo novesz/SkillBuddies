8cd670f7b92b2f535373bc0d015499a38b03f2eb
SzHubi <SzaHub608@hengersor.hu>
2025-11-04 11:10:37 +0100

    Hubi feladata
    login page weboldal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-
+import LoginPage from './pages/LoginPage'
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   )
