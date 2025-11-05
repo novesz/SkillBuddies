589683c6b26c674a6186729314d6a9bb83ce7154
SzHubi <SzaHub608@hengersor.hu>
2025-11-05 10:34:11 +0100

    register változtatás

--- frontend/src/pages/RegistPage.jsx
+++ frontend/src/pages/RegistPage.jsx
@@ -3,22 +3,25 @@ import "../styles/LoginPage.css";
 import Header from "../components/header/Header";
 
 export default function LoginPage() {
-    
-    
   return (
-    
     <div className="login-page">
-         <Header />
+      <Header />
       <div className="login-container">
         <h2 className="login-title">REGISTER</h2>
         <form className="login-form">
-          <input type="text" placeholder="Username" required />
+          
+          <div className="name-row">
+            <input type="text" placeholder="First name" required />
+            <input type="text" placeholder="Second name" required />
+          </div>
+
           <input type="text" placeholder="Email" required />
           <input type="password" placeholder="Password" required />
           <input type="password" placeholder="Password again" required />
-          <button type="submit" className="login-button">REGIST</button>
-          
+
+          <button type="submit" className="login-button">REGISTER</button>
         </form>
+
         <div className="login-links">
           <a href="#">Forgot password?</a>
           <a href="/login">Already have an account? Log in</a>
@@ -27,4 +30,3 @@ export default function LoginPage() {
     </div>
   );
 }
-
