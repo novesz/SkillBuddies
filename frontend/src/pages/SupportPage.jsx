6c95bc997d30be74bb77e58c203358dbe835d994
Pallaga Pál <115153237+palpal828@users.noreply.github.com>
2025-11-06 13:43:29 +0100

    support folytat
    Eszti fejezd be légyszi mert nem bírom a Home.css-t

--- frontend/src/pages/SupportPage.jsx
+++ frontend/src/pages/SupportPage.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/header/Header";
-import "../styles/SupportPage.css";
+import "../styles/Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function SupportPage() {
     return (
         <>
-            <div className="support-page">
+            <div className="sb-page">
                 <Header />
-                <div>
+                <div className="support-content text-center mt-5" id="suppMain">
                     <h1>Support Page</h1>
                     <h2>Get a ticket and our support team will help you as soon as possible</h2>
-                    <form className="support-form">
+                    <form className="support-form col-6 mx-auto mt-4">
                         <label>
                             Email:
-                            <input type="email" name="email" required />
+                            <br />
+                            <input type="email" name="email" required id="emailField" placeholder="email@example.com"/>
                         </label>
                         <br />
                         <label>
                             Issue:
-                            <input name="issue" required></input>
+                            <br />
+                            <textarea name="issue" required id="desrField" placeholder="Describe here"></textarea>
                         </label>
                         <br />
-                        <button type="submit">Submit Ticket</button>
+                        <button type="submit" id="subButton">Submit Ticket</button>
                     </form>
                 </div>
             </div>  
