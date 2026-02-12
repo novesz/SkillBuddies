00fcbb2b9e312cc9ebc8563187a8c5367734dca4
SzHubi <SzaHub608@hengersor.hu>
2026-02-12 10:04:21 +0100

    admin panel kezdet

---
+++ asztali/AdminPanel/AdminPanel/ApiService.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using AdminPanel.Models;
+
+namespace AdminPanel
+{
+    public class ApiService
+    {
+        private readonly HttpClient _client;
+
+        public ApiService()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:3001/") // A backend címe
+            };
+        }
+
+        // Bejelentkezés ellenőrzése
+        public async Task<User?> LoginAsync(string username, string password)
+        {
+            var response = await _client.GetAsync($"users/login?username={username}&password={password}");
+            if (!response.IsSuccessStatusCode) return null;
+
+            return await response.Content.ReadFromJsonAsync<User>();
+        }
+
+        // Felhasználók lekérése
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _client.GetFromJsonAsync<List<User>>("users/all");
+        }
+
+        // Chatek lekérése
+        public async Task<List<Chat>> GetAllChatsAsync()
+        {
+            return await _client.GetFromJsonAsync<List<Chat>>("chats/all");
+        }
+
+        // Chat tagok lekérése
+        public async Task<List<User>> GetUsersInChatAsync(int chatId)
+        {
+            return await _client.GetFromJsonAsync<List<User>>($"chats/chatUsers/{chatId}");
+        }
+        public async Task<List<Message>> GetMessagesInChatAsync(int chatId)
+        {
+            return await _client.GetFromJsonAsync<List<Message>>($"messages/{chatId}");
+        }
+    }
+}
---
+++ asztali/AdminPanel/AdminPanel/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using AdminPanel.Models;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AdminPanel
+{
+    public partial class MainWindow : Window
+    {
+        private ApiService _api = new ApiService();
+        private User _loggedUser;
+
+        public MainWindow(User user)
+        {
+            InitializeComponent();
+            _loggedUser = user;
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            // Felhasználók betöltése
+            var users = await _api.GetAllUsersAsync();
+            UsersGrid.ItemsSource = users;
+
+            // Chatek betöltése
+            var chats = await _api.GetAllChatsAsync();
+            ChatsGrid.ItemsSource = chats;
+        }
+
+        // CHAT KIVÁLASZTÁS
+        private async void ChatsGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ChatsGrid.SelectedItem is Chat selectedChat)
+            {
+                // Chat tagok betöltése
+                var chatUsers = await _api.GetUsersInChatAsync(selectedChat.ChatID);
+                ChatUsersGrid.ItemsSource = chatUsers;
+
+                // Üzenetek betöltése
+                var messages = await _api.GetMessagesInChatAsync(selectedChat.ChatID);
+                MessagesGrid.ItemsSource = messages;
+            }
+        }
+    }
+}
